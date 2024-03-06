d6b77902870fcee3667d3414121141afcc02a1ab
Maya Ghamloush <maya.ghamloushe@gmail.com>
2024-03-06 20:09:20 +0100

    storing readind deleting on apple watch

--- Notes Watch App/View/ContentView.swift
+++ Notes Watch App/View/ContentView.swift
@@ -12,8 +12,44 @@ struct ContentView: View {
     @State private var notes: [Note] = [Note]()
     @State private var text: String = ""
     //MARK: - FUNCTIONS
+    func delete(offsets: IndexSet){
+        withAnimation{
+            notes.remove(atOffsets: offsets)
+            save()
+        }
+    }
+    
+    
     func save(){
-        dump(notes)
+    
+        do{
+           //1. Covet the note array to data using JSON ENCoder
+            let data = try? JSONEncoder().encode(notes)
+            //2. Create a new url to save the filr using the getDocumentDirectory
+            let url = getDocumentDirectory().appendingPathComponent("notes")
+            //3. write the data to the given URL
+            try data?.write(to: url)
+        }catch{
+          print("Saving data has failed")
+        }
+    }
+    func load(){
+        DispatchQueue.main.async {
+            do{
+            //1.get notes urlPAth
+                let url = getDocumentDirectory().appendingPathComponent("notes")
+            //2.create a new property for the data
+                let data = try Data(contentsOf: url)
+                //3.decode the data
+                notes = try JSONDecoder().decode([Note].self, from: data)
+            }catch{
+                print("cannot load any data ")
+            }
+        }
+    }
+    func getDocumentDirectory() -> URL{
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return path[0]
     }
     //MARK: - BODY
     var body: some View {
@@ -39,9 +75,38 @@ struct ContentView: View {
                     .font(.system(size: 42, weight: .semibold))
                 }//hstak
                 Spacer()
-                Text("\(notes.count)")
+                
+                
+                if notes.count >= 1 {
+                    List{
+                        ForEach(0..<notes.count, id: \.self){ i in
+                            HStack{
+                                Capsule()
+                                    .frame(width: 4)
+                                    .foregroundColor(.accentColor)
+                                Text(notes[i].text)
+                                    .lineLimit(1)
+                                    .padding(.leading,5)
+                            }//Hstack
+                        }//LOOP
+                        .onDelete(perform: delete)
+                    }
+                } else {
+                    Spacer()
+                    Image(systemName: "note.text")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundColor(.gray)
+                        .opacity(0.25)
+                        .padding(25)
+                    Spacer()
+                }//END OF LIST
+              
             }//VSTACK
             .navigationTitle("Notes")
+            .onAppear(perform: {
+                load()
+            })
         }
          
     }
